a45ade986583b924ff20a600432cf7ea18854747
sayfullaev3 <sayfullaevjasa22@gmail.com>
2024-08-15 10:29:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ switch (month) {
     case 8:
     case 10:
     case 12:
-        console.log("В этот месяц 31 дней");
+        alert("В этот месяц 31 дней");
         break;
     case 2:
-        console.log("В этот месяц 29 дней");
+        alert("В этот месяц 29 дней");
         break;
     case 4:
     case 6:
     case 9:
     case 11:
-        console.log("В этот месяц 30 дней");
+        alert("В этот месяц 30 дней");
         break;
     default:
         console.log("Пожалуйста введите число 1-12 ");
